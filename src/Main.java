d66aec7f16bc09da8f287a8d60158cbf9d554579
Radhika Bitla <bitlaradhika3@gmail.com>
2023-04-09 21:38:49 +0530

    start

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        //create a bank app
+        // 1. hi
+        // 2. bye
+        // 3. hello
+        // 4. goodbye
+        // 5. hello again!
+        System.out.println("Hello world!");
+
+    }
+}
